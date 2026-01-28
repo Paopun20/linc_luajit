04e8d26cf278ccb32a5e7e472506b42e95921134
Paopun20 <125128258+Paopun20@users.noreply.github.com>
2026-01-28 23:38:46 +0700

    Refactor Linc macro code for clarity

--- test/Test.hx
+++ test/Test.hx
@@ -5,25 +5,20 @@ import llua.Convert;
 
 class Testing {
 	var x = 1;
-	function die(){
+
+	function die() {
 		trace('no way i died');
 	}
 }
 
-
 class Test {
-		
-
 	static function main() {
-
 		var vm:State = LuaL.newstate();
 		LuaL.openlibs(vm);
 		trace("Lua version: " + Lua.version());
 		trace("LuaJIT version: " + Lua.versionJIT());
 
 		LuaL.dofile(vm, "script.lua");
-
-
 
 		// Lua.getglobal(vm, "foo");
 		// Lua.pushinteger(vm, 1);
@@ -33,29 +28,28 @@ class Test {
 			["Int", 1],
 			["Float", 2.0],
 			["String", "three"],
-			["Array[1]", ["four"],1],
-			["Map.v", ["v"=>"five"],"v"],
-			["Anon.v", {"v":"six"},"v"],
+			["Array[1]", ["four"], 1],
+			["Map.v", ["v" => "five"], "v"],
+			[
+				"Anon.v",
+				{
+					"v": "six"
+				},
+				"v"
+			],
 		];
 		trace("from haxe:");
-		trace(Convert.callLuaFunction(vm,'fromHaxe',[testValues]));
+		trace(Convert.callLuaFunction(vm, 'fromHaxe', [testValues]));
 		trace("to haxe:");
-		trace(Convert.callLuaFunction(vm,'toHaxe',null,true));
+		trace(Convert.callLuaFunction(vm, 'toHaxe', null, true));
 
-
-		Convert.setGlobal(vm,"haxeValue",Testing);
-		Convert.callLuaFuncNoReturns(vm,'test');
+		Convert.setGlobal(vm, "haxeValue", Testing);
+		Convert.callLuaFuncNoReturns(vm, 'test');
 		// Convert.toLua(vm,testValues);
 
 		// Lua.pcall(vm,Lua.gettop(vm)-1, 0, 1);
 		// trace('${Convert.fromLua(vm,0)}');
 
-
-
-
 		Lua.close(vm);
-
 	}
-
-
 }
